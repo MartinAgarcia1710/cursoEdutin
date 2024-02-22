fedf09e2c97424356fe7b123f6ca4a1149ada7f3
MartinAgarcia1710 <martindlcg@gmail.com>
2024-02-22 10:40:27 -0300

    Empieza el cuentaVentas

--- CuentaVentasMensual.py
+++ CuentaVentasMensual.py
@@ -1,3 +1,9 @@
+
+ventasMensuales = {
+    "Enero": 0,
+    "Febrero": 0,
+}
+
 print("CUENTA VENTAS ANUAL")
 varAux = 1;
 acumuladorMensual = []
@@ -5,7 +11,8 @@ acumuladorMensual = []
 while varAux != 0:
     montoVenta = float(input("Ingrese el monto de la venta\n"))
     mesVenta = int(input("Ingrese el mes de la venta\n"))
-    acumuladorMensual[mesVenta - 1] += montoVenta
+    if mesVenta == 1:
+        ventasMensuales["Enero"] += montoVenta
     varAux = int(input("Desea seguir cargando ventas? (0: No, 1: Si\n"))
 
-print(acumuladorMensual)
+print(ventasMensuales)
