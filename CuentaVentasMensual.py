0fff7dfeed36a1418b819d8969ee1e2de7a41151
MartinAgarcia1710 <martindlcg@gmail.com>
2024-02-22 16:56:55 -0300

    Empieza el cuentaVentas

--- CuentaVentasMensual.py
+++ CuentaVentasMensual.py
@@ -1,18 +1,38 @@
 
 ventasMensuales = {
-    "Enero": 0,
-    "Febrero": 0,
+    1 : 0,
+    2 : 0,
+    3 : 0,
+    4 : 0,
+    5 : 0,
+    6 : 0,
+    7 : 0,
+    8 : 0,
+    9 : 0,
+    10: 0,
+    11: 0,
+    12: 0,
 }
 
 print("CUENTA VENTAS ANUAL")
-varAux = 1;
-acumuladorMensual = []
 
-while varAux != 0:
-    montoVenta = float(input("Ingrese el monto de la venta\n"))
-    mesVenta = int(input("Ingrese el mes de la venta\n"))
-    if mesVenta == 1:
-        ventasMensuales["Enero"] += montoVenta
-    varAux = int(input("Desea seguir cargando ventas? (0: No, 1: Si\n"))
+while True:
+    print("1. Cargar ventas\n2. Recaudación mensual\n3. Salir\n")
+    opcion = int(input("Seleccionar opcion:\n"))
+    if opcion == 1:
+        numeroCliente = 1
+        print("Para finalizar la carga de ventas indique numero de cliente <0>\n")
+        numeroCliente = int(input("Ingrese numero de cliente\n"))
+        while numeroCliente != 0:
+            montoVenta = float(input("Ingrese el monto de la venta\n"))
+            mesVenta = int(input("Ingrese el mes de la venta\n"))
+            numeroCliente = int(input("Ingrese numero de cliente\n"))
+            ventasMensuales[mesVenta] += montoVenta
 
-print(ventasMensuales)
+    elif opcion == 2:
+        for x in ventasMensuales:
+            print(ventasMensuales[x])
+    elif opcion == 3:
+        exit()
+
+#print(ventasMensuales)
